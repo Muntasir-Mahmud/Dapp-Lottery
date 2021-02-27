0a6fcf2470e3a7cf426bdc8ef0a9f5201d82ba81
Muntasir-Mahmud <m.siyam529@gmail.com>
2021-02-27 18:07:05 +0600

    test multiple account enterance

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -34,4 +34,30 @@ describe('lottery Contract', () => {
         assert.equal(accounts[0], players[0]);
         assert.equal(1, players.length);
     });
+
+    it('allows multiplw accounts to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.2', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.2', 'ether')
+        });
+
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.2', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length);
+    });
 });
